7ceba4fffd2d084494c5225a0753b1e07fa934f8
ivan flores <gtzf.ivan@gmail.com>
2025-09-28 23:11:21 -0600

    Version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 1
-        versionName = "1.2"
+        versionName = "1.3"
 
         manifestPlaceholders["mapsApiKey"] = mapsApiKey
 
